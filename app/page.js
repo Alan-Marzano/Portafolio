b15c7f6176c20cadfcbf26fde7551e4a8a0cca1e
alan <nvvalanlp@gmail.com>
2024-01-11 18:49:03 -0300

    portfolio

--- app/page.js
+++ app/page.js
@@ -5,8 +5,6 @@ import Habilidades from './componentes/Habilidades'
 import Portafolio from './componentes/Portafolio'
 import Contacto from './componentes/Contacto'
 
-
-
 export default function Home() {
   return (
     <div>
